03d70ddebf4638469488bd2f0f78bee62d86dde6
Ondrej Kaspar <ondrej.kaspar@lmc.eu>
2021-04-29 09:18:58 +0200

    break HP to smaller components, add styles

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './menu-items.styles.scss';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div className={`menu-item ${size}`}>
+        <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+        <div className="content">
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">Shop now</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
