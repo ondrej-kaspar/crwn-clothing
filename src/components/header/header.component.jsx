812f03999474bedf88c5caedd8d52c468a4b034a
Ondrej Kaspar <ondrej.kaspar@lmc.eu>
2021-05-19 13:17:45 +0200

    convert styles to styled components for homepage, header and custom button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -11,27 +10,27 @@ import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className="logo" />
-        </Link>
-        <div className="options">
-            <Link to="/shop" className="option">Shop</Link>
-            <Link to="/contact" className="option">Contact</Link>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to="/shop">Shop</OptionLink>
+            <OptionLink to="/contact">Contact</OptionLink>
             {
                 currentUser ?
-                    <button type="button" className="option" onClick={() => auth.signOut()}>Sign out</button>
+                    <OptionLink as="button" type="button" onClick={() => auth.signOut()}>Sign out</OptionLink>
                     :
-                    <Link to="/signin" className="option">Sign in</Link>
+                    <OptionLink to="/signin">Sign in</OptionLink>
 
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         { hidden ? null : <CartDropdown /> }
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
